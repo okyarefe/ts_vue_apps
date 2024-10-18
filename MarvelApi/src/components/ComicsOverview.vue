879f8314dfcb5519ea6a8c693bbb48445f53f088
efe okyar <f.efe.okyar@gmail.com>
2024-10-18 12:03:48 +0300

    implemented pagination component PaginationComics

--- MarvelApi/src/components/ComicsOverview.vue
+++ MarvelApi/src/components/ComicsOverview.vue
@@ -1,20 +1,58 @@
 <script setup lang="ts">
 import LoadingIndicator from "./LoadingIndicator.vue";
 import { useComics } from "@/composables/useFetch";
+import { useRoute } from "vue-router";
 import type { Comic } from "@/types";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import ComicCard from "./ComicCard.vue";
+import PaginationComics from "./PaginationComics.vue";
 
+const route = useRoute();
 const comicsRef = ref<Comic[]>();
 const isLoading = ref<boolean>(false);
+const currentPage = ref<number | string>(0);
+const totalPages = ref<number>(0);
+
+// get the page variable from router and convert to a number +=
+if (route.params.page) {
+  currentPage.value = +route.params.page;
+  console.log("Page number from URL:", currentPage.value);
+}
 
 const getComics = async () => {
+  console.log("IN FUNC", typeof currentPage.value);
+
   isLoading.value = true;
-  const comics = await useComics();
-  console.log("Comics Returned from backend", comics);
+
+  const comics = await useComics(currentPage.value);
+  currentPage.value = comics?.offset / comics?.limit || 0;
+  totalPages.value = Math.ceil(comics.total / comics.limit);
+
   comicsRef.value = comics.results;
   isLoading.value = false;
 };
+
+watch(
+  () => route.params.page,
+  async (newPage, oldPage) => {
+    console.log("OLD VALUE:", oldPage);
+    console.log("NEW VALUE", newPage);
+
+    console.log("TYPE OF OLD VALUE", typeof newPage);
+    if (!newPage) {
+      console.log("YEP!");
+      currentPage.value = 0;
+    } else if (newPage) {
+      currentPage.value = Number(newPage);
+    }
+    console.log("We are at page :", currentPage.value);
+    console.log("Type of this :", typeof currentPage.value);
+    // const pageNumber = Number(newPage);
+    // currentPage.value = pageNumber; // Update `currentPage` ref
+    await getComics();
+  },
+  { immediate: true }
+);
 
 onMounted(async () => {
   await getComics();
@@ -36,6 +74,12 @@ onMounted(async () => {
           v-for="comic in comicsRef"
           :comic="comic"
         ></ComicCard>
+        <PaginationComics
+          :total-pages="totalPages"
+          :current-page="+currentPage"
+          path="/"
+        >
+        </PaginationComics>
       </div>
     </div>
   </div>
--- MarvelApi/src/composables/useFetch.ts
+++ MarvelApi/src/composables/useFetch.ts
@@ -2,8 +2,8 @@ import type { Comics } from "@/types";
 
 const BACKEND_END_POINT = "http://localhost:5000/marvel";
 
-export const useComics = async (): Promise<Comics> => {
-  const response = await fetch(BACKEND_END_POINT);
+export const useComics = async (page: number): Promise<Comics> => {
+  const response = await fetch(`${BACKEND_END_POINT}?page=${page}`);
   const data = await response.json();
   console.log("Data from backend:", data);
   return data;
